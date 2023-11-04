904391ce3896604a0b2e0149324e718a511086b9
Kaveesh028 <130683460+Kaveesh028@users.noreply.github.com>
2023-11-04 08:38:38 +0530

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,1 +1,16 @@
 
+module.exports = {
+  pairing_mode: true,
+  session_folder_name: 'session',
+  browser: ["Chrome (Windows)", "latest"],
+  public_mode: true,
+  prefix: '.',
+  bot: {
+    name: 'Bot Name',
+    profile_status: 'Bot Status'
+  },
+  owner: {
+    name: ["Owner Name"],
+    number: ["628xxx"]
+  }
+};
